336a9081d2c95e1d243036108dc84e666ad761db
Travis CI <builds@travis-ci.com>
2018-11-29 11:49:10 -0600

    return TextArea Node
    ALlows me to capture the TextArea Node so I can pritn from it.

--- src/GuiObjects.java
+++ src/GuiObjects.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
+ * 
+ * 
  */
 
 /**
@@ -90,6 +90,11 @@ public class GuiObjects {
     public String retrieveTextAreaData(){
         
        return ta_text.getText();
+    }
+    
+    public TextArea TextAreaToBePrinted(TextArea t){
+       t = ta_text;
+       return t;
     }
     
     
